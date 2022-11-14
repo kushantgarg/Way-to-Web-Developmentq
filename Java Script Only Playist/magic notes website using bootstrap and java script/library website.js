d2d39ad7150a6938dd377e42a42cffc9c587e74a
Kushant Garg <71939372+kushantgarg@users.noreply.github.com>
2022-11-14 23:25:11 +0530

    Add files via upload

---
+++ Java Script Only Playist/magic notes website using bootstrap and java script/library website.js
@@ -0,0 +1,124 @@
+console.log("This is index.js");
+// Todos"
+// 1. Store all the data to the localStorage
+// 2. Give another column as an option to delete the book
+// 3. Add a scroll bar to the view
+
+// Constructor
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+// Display Constructor
+function Display() {
+
+}
+
+// Add methods to display prototype
+Display.prototype.add = function (book) {
+    console.log("Adding to UI");
+    tableBody = document.getElementById('tableBody');
+     let books=localStorage.getItem("books");
+    if(books==null)
+    {
+        booksobj=[];
+    }
+    else
+    {
+        booksobj=JSON.parse(books);
+    }
+  booksobj.forEach(element => {
+    let uiString = `<tr>
+    <td>${element.name}</td>
+    <td>${element.author}</td>
+    <td>${element.type}</td>
+</tr>`; 
+tableBody.innerHTML+=uiString;  
+  });
+ 
+
+}
+
+// Implement the clear function
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm');
+    libraryForm.reset();
+}
+
+// Implement the validate function
+Display.prototype.validate = function (book) {
+    if (book.name.length < 2 || book.author.length < 2) {
+        return false
+    }
+    else {
+        return true;
+    }
+}
+Display.prototype.show = function (type, displayMessage) {
+    let message = document.getElementById('message');
+    message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Messge:</strong> ${displayMessage}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                            </button>
+                        </div>`;
+    setTimeout(function () {
+        message.innerHTML = ''
+    }, 2000);
+
+}
+
+
+// Add submit event listener to libraryForm
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log('YOu have submitted library form');
+    let books=localStorage.getItem("books");
+    if(books==null)
+    {
+        booksobj=[];
+    }
+    else
+    {
+        booksobj=JSON.parse(books);
+    }
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+    let type;
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+
+    let book = new Book(name, author, type);
+    booksobj.push( book );
+    console.log(book);
+localStorage.setItem("books",JSON.stringify(booksobj));
+    let display = new Display();
+
+    if (display.validate(book)) {
+
+        display.add(book);
+        display.clear();
+        display.show('success', 'Your book has been successfully added')
+    }
+    else {
+        // Show error to the user
+        display.show('danger', 'Sorry you cannot add this book');
+    }
+
+    e.preventDefault();
+}
---
+++ Java Script Only Playist/magic notes website using bootstrap and java script/magicnotes.js
@@ -0,0 +1,139 @@
+console.log("Welcome to Magic Notes");
+showNotes();
+let addBtn=document.getElementById("addBtn");
+addBtn.addEventListener('click',function(e){
+    // let notesObj;
+    let addtitle=document.getElementById('title');
+    let addTxt=document.getElementById('addTxt');
+    // i am trying to retireve the notes that is stored in loacal storage
+//  if the notes key is NULL then we are creating a new array to store the notes in it 
+let title=localStorage.getItem("title");
+if(title==null)
+{
+    titleObj=[];
+} 
+// if the notes is not null then we are just storing it in our array after parsing into a object format
+else
+{
+    // console.log(notes);
+    titleObj=JSON.parse(title);
+}
+    let notes=localStorage.getItem("notes");
+    if(notes==null)
+    {
+        notesObj=[];
+    } 
+    // if the notes is not null then we are just storing it in our array after parsing into a object format
+    else
+    {
+        // console.log(notes);
+        notesObj=JSON.parse(notes);
+    }
+    //  after it we are pushing the notes in the array
+    notesObj.push(addTxt.value);
+    titleObj.push(addtitle.value);
+    localStorage.setItem("notes",JSON.stringify(notesObj));
+    localStorage.setItem("title",JSON.stringify(titleObj))
+    addTxt=" " ;
+    addtitle=" ";
+console.log(notesObj);
+    // console.log(notes);
+    // console.log(title);
+    showNotes();
+
+});
+
+//  grabbing the title value of the notes
+
+function showNotes(){
+    let title=localStorage.getItem("title");
+    if(title==null)
+    {
+        titleObj=[];
+    } 
+    // if the notes is not null then we are just storing it in our array after parsing into a object format
+    else
+    {
+        // console.log(notes);
+        titleObj=JSON.parse(title);
+    }
+
+    let notes=localStorage.getItem("notes");
+    if(notes==null)
+    {
+        notesObj=[];
+    } 
+    // if the notes is not null then we are just storing it in our array after parsing into a object format
+    else
+    {
+        // console.log(notes);
+        notesObj=JSON.parse(notes);
+    }
+     
+    let html="";
+    notesObj.forEach(function(element,index){
+        html+=`
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title font-weight-bold">${titleObj[index]}</h5>
+                    <p class="card-text"> ${element}</p>
+                    <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                </div>
+            </div>`;
+    });
+    let notesElem=document.getElementById("notes");
+    if(notesObj.length!=0)
+    {
+        notesElem.innerHTML=html;
+    }
+    else
+    notesElem.innerHTML='Nothing to show right now, Use Add Note functionality to add a Note.';
+}
+//  addingdelete note functionality 
+function deleteNote(index)
+{
+    let title=localStorage.getItem("title");
+    if(title==null)
+    {
+        titleObj=[];
+    } 
+    // if the notes is not null then we are just storing it in our array after parsing into a object format
+    else
+    {
+        // console.log(notes);
+        titleObj=JSON.parse(title);
+    }
+
+    let notes=localStorage.getItem("notes");
+    if(notes==null)
+    {
+        notesObj=[];
+    } 
+    // if the notes is not null then we are just storing it in our array after parsing into a object format
+    else
+    {
+        // console.log(notes);
+        notesObj=JSON.parse(notes);
+    }
+    titleObj.splice(index,1);
+    notesObj.splice(index,1);
+    localStorage.setItem("title",JSON.stringify(titleObj));
+    localStorage.setItem("notes",JSON.stringify(notesObj));
+    showNotes();
+}
+//  adding search functionaity
+let search=document.getElementById("searchTxt");
+search.addEventListener('input',function(){
+    let inputVal=search.value.toLowerCase();
+    let notesCard=document.getElementsByClassName('noteCard');
+    Array.from(notesCard).forEach(function(element){
+        let cardtxt=element.getElementsByTagName("p")[0].innerText;
+        if(cardtxt.includes(inputVal))
+        element.style.display="block";
+         else
+         element.style.display="none";
+
+
+    })
+
+})
